aa05ed7adf9e318953c404ff8c0d77adc361af14
cheja1018 <72240876+cheja1018@users.noreply.github.com>
2022-06-21 09:25:45 +1200

    Skeleton of the base code

---
+++ 00_base_v0.py
@@ -0,0 +1,15 @@
+"""
+base component for the game Lucky Unicorn
+v0 - skeleton setup
+
+author - Jasmine Chen
+CC JC 2022
+
+"""
+
+# import libraries below this ine *****************
+
+# all functions  below this line *****************
+
+# main routine below this line *****************
+print("Hello")
